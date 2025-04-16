1f6744a980c6d39f7784f9d15d2a431d07ae546a
Lushbits <lushbits@gmail.com>
2025-04-16 14:17:00 +0200

    added owner user id string

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string
   readonly VITE_SUPABASE_ANON_KEY: string
   readonly VITE_APP_VERSION?: string
+  readonly VITE_OWNER_USER_ID: string
 }
 
 interface ImportMeta {
